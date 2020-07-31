6ec26d7bd517a2b411348ed54fad03087449d1ca
James Van Slyke <60765773+jakevs@users.noreply.github.com>
2020-07-30 22:56:13 -0500

    got results to render to page

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
 import Row from "./Row";
-import getEmployees from "../utils/Api";
+import axios from "axios";
+// import getEmployees from "../utils/Api";
 import {
   Card,
   CardImg,
@@ -11,55 +12,67 @@ import {
   Table
 } from "reactstrap";
 
-const ExampleCard = async () => {
-  // const data = await getEmployees;
-  let data = [];
-  data = getEmployees();
-  console.log(data);
+class ExampleCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      employees: []
+    };
+  }
 
-  return (
-    <div>
-      <Card>
-        <CardImg
-          top
-          width="100%"
-          src="https://cdn.iveybusinessjournal.com/wp-content/uploads/2006/03/iStock_000012204568_Large.jpg"
-          alt="Card image cap"
-        />
-        <CardBody>
-          <CardTitle>Employee Directory</CardTitle>
-          <Table dark>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Username</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data.map((obj, i) => {
-                return (
-                  <Row
-                    key={i}
-                    id={obj.id}
-                    firstName={obj.firstName}
-                    lastName={obj.lastName}
-                    username={obj.username}
-                  />
-                );
-              })}
-            </tbody>
-          </Table>
-          <CardText>
-            Use the search button to find information about individual employees
-            at our company!
-          </CardText>
-          <Button>Search Directory</Button>
-        </CardBody>
-      </Card>
-    </div>
-  );
-};
+  componentDidMount() {
+    axios.get("https://randomuser.me/api/?results=10").then((res) => {
+      let results = res.data.results;
+      console.log(results);
+      this.setState({ employees: results });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Card>
+          <CardImg
+            top
+            width="100%"
+            src="https://cdn.iveybusinessjournal.com/wp-content/uploads/2006/03/iStock_000012204568_Large.jpg"
+            alt="Card image cap"
+          />
+          <CardBody>
+            <CardTitle>Employee Directory</CardTitle>
+            <Table dark>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Username</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.employees.map((obj, i) => {
+                  return (
+                    <Row
+                      key={obj.login.uuid}
+                      id={obj.login.uuid}
+                      firstName={obj.name.first}
+                      lastName={obj.name.last}
+                      username={obj.login.username}
+                    />
+                  );
+                })}
+              </tbody>
+            </Table>
+            <CardText>
+              Use the search button to find information about individual
+              employees at our company!
+            </CardText>
+            <Button>Search Directory</Button>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
+}
 
 export default ExampleCard;
--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,16 +1,24 @@
 // import React from "react";
 import axios from "axios";
+import util from "util";
+// const axiosGet = util.promisify(axios.get);
 
-const getEmployees = () => {
-  axios.get("https://randomuser.me/api/?results=10").then((res) => {
-    const persons = res.data.results;
-    console.log(persons);
+// const getEmployees = () => {
+//   axios.get("https://randomuser.me/api/?results=10").then((res) => {
+//     const persons = res.data.results;
+//     console.log(persons);
 
-    // this.setState({ persons });
-    return persons;
-  });
-};
-export default getEmployees;
+//     // this.setState({ persons });
+//     return;
+//   });
+// };
+
+// const getEmployees = async () => {
+//   // const employees = await axiosGet("https://randomuser.me/api/?results=10");
+
+//   return employees.data.results;
+// };
+// export default getEmployees;
 
 // export default class getEmployees extends React.Component {
 //   state = {
